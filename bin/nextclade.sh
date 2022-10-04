b9ef7975bc698419128230522b8258196d0d947b
rebeelouise <hlrpenri@liverpool.ac.uk>
2022-10-04 16:57:25 +0100

    nextclade script update due to version update

--- bin/nextclade.sh
+++ bin/nextclade.sh
@@ -11,4 +11,12 @@ output=$2
 
 mkdir -p $output
 
-nextclade --input-fasta=${fasta} --input-root-seq=/home/rebee/tools/artic-ncov2019/primer_schemes/nCoV-2019/V3/nCoV-2019.reference.fasta --genes=E,M,N,ORF1a,ORF1b,ORF3a,ORF6,ORF7a,ORF7b,ORF8,ORF9b,S --input-gene-map=/home/rebee/tools/nextclade/data/sars-cov-2/genemap.gff --input-tree=/home/rebee/tools/nextclade/data/sars-cov-2/tree.json --input-qc-config=/home/rebee/tools/nextclade/data/sars-cov-2/qc.json --output-json=${output}/nextclade.json --output-csv=${output}/nextclade.csv --output-tsv=${output}/nextclade.tsv --output-tree=${output}/nextclade.auspice.json --output-dir=${output}/ --output-basename=nextclade
+nextclade \
+		--input-fasta=${fasta} \
+			--input-dataset=/home/rebee/tools/nextclade/data/sars-cov-2 \
+				--output-json=${output}/nextclade.json \
+					--output-csv=${output}/nextclade.csv \
+						--output-tsv=${output}/nextclade.tsv \
+							--output-tree=${output}/nextclade.auspice.json \
+								--output-dir=${output}/ \
+									--output-basename=nextclade
